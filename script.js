5cdabaa9aacb2dade44f0434277bdb9b617b9f6e
Nish Mitchell <shanishamit@gmail.com>
2021-07-01 19:26:39 -0400

    adding my files to new repo

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+
+
+document.getElementById('button1').onclick = function() {
+  location.href = "https://drive.google.com/file/d/1nybpo-0EqIdanchlf5tGfLVDyscqyLl4/view?usp=sharing";
+
+};
+
+document.getElementById('button2').onclick = function() {
+  location.href = "https://nishyo-coder.github.io/Project-One-Pandemic-Travel/";
+
+};
+
+document.getElementById('button3').onclick = function() {
+  location.href = "https://storybook-weddings.herokuapp.com/";
+
+};
+
+
